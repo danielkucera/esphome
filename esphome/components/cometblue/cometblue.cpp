98ddbb295a656f7db13b04de88ac822fb77c49b8
Daniel Kucera <daniel.kucera@gmail.com>
2019-11-13 23:10:26 +0100

    actions, temp setting, interval

--- esphome/components/cometblue/cometblue.cpp
+++ esphome/components/cometblue/cometblue.cpp
@@ -15,11 +15,12 @@ CometBlue* dev;
 climate::ClimateTraits CometblueClimate::traits() {
   auto traits = climate::ClimateTraits();
   traits.set_supports_current_temperature(true);
-  traits.set_supports_auto_mode(true);
+  traits.set_supports_auto_mode(false);
   traits.set_supports_cool_mode(false);
   traits.set_supports_heat_mode(true);
   traits.set_supports_two_point_target_temperature(false);
   traits.set_supports_away(false);
+  traits.set_supports_action(true);
   traits.set_visual_min_temperature(COMETBLUE_TEMP_MIN);
   traits.set_visual_max_temperature(COMETBLUE_TEMP_MAX);
   traits.set_visual_temperature_step(0.5);
@@ -28,8 +29,8 @@ climate::ClimateTraits CometblueClimate::traits() {
 
 int CometblueClimate::connect() {
   if (!dev->isConnected() && !dev->connect(mac_, 0)){
-  	ESP_LOGCONFIG(TAG, "Connect to '%s' FAILED!", this->mac_.c_str());
-	return 0; //why are we void?
+    ESP_LOGCONFIG(TAG, "Connect to '%s' FAILED!", this->mac_.c_str());
+    return 0;
   }
   ESP_LOGCONFIG(TAG, "Connected to '%s' '%s'", this->mac_.c_str(), dev->getModelNumber().c_str());
 
@@ -39,61 +40,55 @@ int CometblueClimate::connect() {
 void CometblueClimate::setup() {
   ESP_LOGCONFIG(TAG, "Setting up CometBlue client for '%s'...", this->mac_.c_str());
 
+  this->mode = climate::CLIMATE_MODE_HEAT; // we can only heat
+  this->current_temperature = NAN;
+  this->target_temperature = NAN;
+
+  //safe to do here?
+  esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
+
   dev = new CometBlue(mac_);
 
-  connect();
+  //TODO: dynamic interval
+  this->set_interval(300*1000, [this] { this->update(); });
 
   update();
-  /*
-  Temperatures temp = dev->getTemperatures();
-
-  this->current_temperature = temp.current_temp;
-  this->publish_state();
-  */
-/*
-  if (this->sensor_) {
-    this->sensor_->add_on_state_callback([this](float state) {
-      this->current_temperature = state;
-      // current temperature changed, publish state
-      this->publish_state();
-    });
-    this->current_temperature = this->sensor_->state;
-  } else
-    this->current_temperature = NAN;
-  // restore set points
-  auto restore = this->restore_state_();
-  if (restore.has_value()) {
-    restore->apply(this);
-  } else {
-    // restore from defaults
-    this->mode = climate::CLIMATE_MODE_OFF;
-    this->target_temperature = 24;
-  }
-  */
 }
 
 void CometblueClimate::update() {
+  ESP_LOGCONFIG(TAG, "Updating state of '%s'...", this->mac_.c_str());
   if (!connect()){
-	  return;
+    return;
   }
 
   Temperatures temp = dev->getTemperatures();
 
   this->current_temperature = temp.current_temp;
+  this->target_temperature = temp.manual_temp;
+
+  if (temp.current_temp < temp.manual_temp){
+    this->action = climate::CLIMATE_ACTION_HEATING;
+  } else {
+//    this->action = climate::CLIMATE_ACTION_IDLE;
+    this->action = climate::CLIMATE_ACTION_OFF;
+  }
+
   this->publish_state();
 }
 
 void CometblueClimate::control(const climate::ClimateCall &call) {
-  update();
-/*
-  if (call.get_mode().has_value())
-    this->mode = *call.get_mode();
-  if (call.get_target_temperature().has_value())
-    this->target_temperature = *call.get_target_temperature();
+  if (call.get_target_temperature().has_value()){
+    if (connect()){
+      Temperatures temp = dev->getTemperatures();
+      temp.manual_temp = *call.get_target_temperature(); // target temperature in degree Celsius
+      ESP_LOGCONFIG(TAG, "About to set target temperature for '%s' to %f", this->mac_.c_str(), temp.manual_temp);
+      if (!dev->setTemperatures(temp)){
+        ESP_LOGCONFIG(TAG, "Seting temperature failed!");
+      }
+    }
+  }
 
-  this->transmit_state_();
-  this->publish_state();
-*/
+  update();
 }
 
 }  // namespace cometblue
